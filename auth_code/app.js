8f143c7178be6f63aad59cc801815b3ffe12b70b
Brad Harris <bradharris@Brad-Harris-MacBook-Air.local>
2017-04-22 00:16:58 -0700

    setting up  express server

---
+++ auth_code/app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var request = require('request');
+var querystring = require('querystring');
+var cookieParser = require('cookie-parser');
+
+// var redirect_uri = 'http://localhost:8888/callback';
+
+// var generateString = function(length) {
+// 	var text = '';
+// 	var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+// 	for (var i = 0; i < length; i++) {
+// 		text += possible.chatAt()
+// 	}
+// }
