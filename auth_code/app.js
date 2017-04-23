a933e02b21dc975348b8fb90ffaafb3381347e66
Brad Harris <bradharris@Brad-Harris-MacBook-Air.local>
2017-04-22 18:19:27 -0700

    up until refresh token line 120 complete

--- auth_code/app.js
+++ auth_code/app.js
@@ -20,22 +20,92 @@ var redirect_uri = 'http://localhost:8888/callback';
 var stateKey = 'spotify_auth_state';
 
 app
-.use(express.statis(__dirname + '/public'))
-.use(cookieParser())
+.use(express.static(__dirname + '/public'))
+.use(cookieParser());
 
+app
 .get('/login', function(req, res) {
 
 	// Use cookies to ensure user didn't just go straight to callback, and in fact received random string after authenticated. Cookies are key/value.
 	var state = generateRandomString(16);
 	res.cookie(stateKey, state);
 
-	// authorization
+	// authorization / request spotify 'code'
 	var scope = 'user-read-private user-read-email user-follow-read user-library-read user-top-read';
+
+	console.log('Authorizing with Spotify, redirecting to https://accounts.spotify.com/authorize?...');
 	res.redirect('https://accounts.spotify.com/authorize?' + querystring.stringify({
-			response_tope = 'code',
+			response_type = 'code',
 			client_id: process.env.CLIENT_ID,
-			csope: scope.
-			redirect_uri:redirect_uri,
+			scope: scope,
+			redirect_uri: redirect_uri,
 			state: state
 		}));
-})
+});
+
+// spotify returns with code, now request access token after checking state parameter
+app
+.get('/callback', function(req, res) {
+
+	/* 
+	WHATS HAPPENING HERE?
+
+	Isn't code and state returned in the response from first call, not request? Or is this building the next request?
+	*/
+	var code = req.query.code || null;
+	var state = req.query.state || null;
+	var storedState = req.cookies ? req.cookies[stateKey] : null;
+
+	if (state === null || state !== storedState) {
+		res.redirect('/#' + querystring.stringify({
+			error: 'state_mismatch'
+		}));
+	} else {
+
+		res.clearCookie(stateKey);
+		var authOptions = {
+			url: 'https://accounts.spotify.com/api/token',
+			// form same as body
+			form: {
+				code: code,
+				redirect_uri: redirect_uri,
+				grant_type: 'authorization_code'
+			},
+			headers: {
+				'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+			},
+			json: true
+		};
+
+		request.post(authOptions, function(err, response, body) {
+			console.log("Requesting access token...");
+
+			if (!error && response.statusCode === 200) {
+
+				console("Access token granted");
+				var access_token = body.access_token;
+				var refresh_token = body.refresh_token;
+
+				var options = {
+					url: 'https://api.spotify.com/v1/me',
+					headers: { 'Authorization': 'Bearar ' + access_token },
+					json: true
+				};
+
+				// use access token
+				request.get(options, function(err, response, body) {
+					console.log("body: ", body);
+				});
+
+			} else {
+				res.redirect('/#' + querystring.stringify({
+					error: 'invalid_token'
+				}));
+			}
+		});
+	}
+});
+
+
+
+
