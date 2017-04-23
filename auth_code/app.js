6a6dca3d81b30c0df8fbca4c3a1fcdac52adb7e5
Brad Harris <bradharris@Brad-Harris-MacBook-Air.local>
2017-04-22 22:47:30 -0700

    added all code

--- auth_code/app.js
+++ auth_code/app.js
@@ -44,8 +44,7 @@ app
 });
 
 // spotify returns with code, now request access token after checking state parameter
-app
-.get('/callback', function(req, res) {
+app.get('/callback', function(req, res) {
 
 	/* 
 	WHATS HAPPENING HERE?
@@ -105,6 +104,33 @@ app
 		});
 	}
 });
+
+app.get('/refresh_token', function(req, res) {
+
+	console.log("Requesting access token from refresh token");
+	var refresh_token = req.query.refresh_token;
+	var authOptions = {
+		url: 'https://accounts.spotify.com/api/token',
+		headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
+		form: {
+			grant_type: 'refresh_token',
+			refresh_token: refresh_token
+		},
+		json: true
+	};
+
+	request.post(authOptions, function(err, response, body) {
+		if (!error && response.statusCode === 200) {
+			var access_token = body.access_token;
+			res.send({
+				'access_token': access_token
+			});
+		}
+	});
+});
+
+console.log("Listening on 8888");
+app.listen(8888);
 
 
 
